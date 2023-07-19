544e92b5a45065bf6fd8eeda66e4a6f63e2efa21
aduralbizu <adur.albizu@gmail.com>
2023-07-19 10:05:55 +0200

    dsd

--- Modulo6/Practica1/Program.cs
+++ Modulo6/Practica1/Program.cs
@@ -1,11 +1,31 @@
-﻿namespace Practica1
+﻿using static Practica1.Vehiculo;
+
+namespace Practica1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var coche1 = new Vehiculo(new DateTime(2006,01,06),"Azul");
+            var coche2 = new Vehiculo( "Wolskwagen","Polo");
+            var coche3 = new Vehiculo(new DateTime(2020,01,06), "Opel","Corsa","Negro");
+            var coche4 = new Vehiculo(new DateTime(2022,01,06), "Nissan","X-trail","Naranja");
 
-            Console.WriteLine("Hello, World!");
+            var lista = new List<Vehiculo>();
+
+            lista.Add(coche1);
+            lista.Add(coche2);
+            lista.Add(coche3);
+            lista.Add(coche4);
+
+            var index = 0;
+            foreach (var coche in lista) {
+                Console.WriteLine("Coche no."+ ++index );
+                coche.imprimirCoche();
+                
+            }
+
+
         }
     }
 }
---
+++ Modulo6/Practica1/Vehiculo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    public class Vehiculo
+    {
+
+        public DateTime AñoCompra { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+   
+
+        public Vehiculo(DateTime añocompra, string marca, string modelo,string color) {
+            AñoCompra = añocompra;
+            Marca = marca;
+            Modelo = modelo;
+            Color = color;
+        } //Constructor
+
+        public Vehiculo(DateTime añocompra, string color)
+        {
+            AñoCompra = añocompra;
+            Color = color;
+        }
+        public Vehiculo( string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
+        public void imprimirCoche()
+        {
+            Console.WriteLine("Año de compra: " + AñoCompra);
+            Console.WriteLine("Marca: " + Marca);
+            Console.WriteLine("Modelo " + Modelo);
+            Console.WriteLine("Color: " + Color);
+            Console.WriteLine();
+        }
+
+
+    }
+}
