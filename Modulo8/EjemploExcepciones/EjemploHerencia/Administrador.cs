e045879b58b3dfb89ee6b30f766fa4d55cff1292
aduralbizu <adur.albizu@gmail.com>
2023-07-21 09:09:10 +0200

    dfdf

---
+++ Modulo8/EjemploExcepciones/EjemploHerencia/Administrador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EjemploHerencia;
+
+namespace EjemploHerencia
+{
+    public class Administrador : Empleado
+    {
+        public Administrador(string nombre) : base(nombre)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"[ Empleado.Nombre: {Nombre}" +
+                $" Dias Vacaciones: {diasVacaciones}" +
+                $" Tipo: Administrativo ]";
+        }
+    }
+}
---
+++ Modulo8/EjemploExcepciones/EjemploHerencia/Empleado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploHerencia
+{
+    public partial class Empleado
+    {
+        public String? Telefono { get; set; }
+
+
+        protected double diasVacaciones;
+
+        public override string ToString()
+        {
+            return $"[ Empleado.Nombre: {Nombre}" +
+                $" Dias Vacaciones: {diasVacaciones}" +
+                $" Tipo: Empleado ]";
+
+        }
+    } 
+}
