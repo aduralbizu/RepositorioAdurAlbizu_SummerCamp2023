ae9aafc65150d4906af4d12fdbb3b1a62ce8e976
aduralbizu <adur.albizu@gmail.com>
2023-07-26 13:18:51 +0200

    nuevo ejerccioc

---
+++ Modulo 81/04-demos/before/02TextFileLines/DataProcessor/JsonFileProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace DataProcessor
+{
+    public class JsonFileProcessor
+    {
+        public string InputFilePath { get; }
+        public string OutputFilePath { get; }
+
+        public JsonFileProcessor(string inputFilePath, string outputFilePath)
+        {
+            InputFilePath = inputFilePath;
+            OutputFilePath = outputFilePath;
+        }
+
+        public void Process()
+        {
+            // Using read all text
+            string originalText = File.ReadAllText(InputFilePath);
+            string processedText = originalText.ToUpperInvariant();
+            File.WriteAllText(OutputFilePath, processedText);
+        }
+    }
+}
---
+++ Modulo 81/04-demos/before/02TextFileLines/ImportarJson/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using DataProcessor;
+using Newtonsoft.Json;
+
+namespace ImportarJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string json = @"{
+             'Name': 'Bad Boys',
+             'ReleaseDate': '1995-4-7T00:00:00',
+             'Genres': [
+                'Action',
+                'Comedy'
+              ]
+             }";
+
+            Movie m = JsonConvert.DeserializeObject<Movie>(json);
+
+            string name = m.Name;
+
+            Console.WriteLine(name);
+            */
+            // Bad Boys
+
+
+           //Ejercicio de hoy:
+           string json = File.ReadAllText(@"C:\prueba2\Modulo 81\04-demos\before\02TextFileLines\ImportarJson\Monedas.json");
+
+           List <Moneda> monedas = JsonConvert.DeserializeObject< List <Moneda>>(json);
+
+           Console.WriteLine("LISTA MONEDAS: \n");
+
+           foreach (Moneda moneda in monedas)
+           {
+                
+               string name = moneda.nombre;
+               string codigo = moneda.codigo;
+               float valorEnDolares = moneda.valorEnDolares;
+               Console.WriteLine($"Name: {name}");
+               Console.WriteLine($"Codigo: {codigo}");
+               Console.WriteLine($"Valor en dólares: {valorEnDolares}");
+               Console.WriteLine();
+                            
+           }
+
+
+        }
+    }
+}
