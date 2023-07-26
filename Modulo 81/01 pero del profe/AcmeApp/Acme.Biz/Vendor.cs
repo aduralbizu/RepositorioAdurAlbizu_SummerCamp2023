3b1001176b7397eab15097b93359f13db980733f
aduralbizu <adur.albizu@gmail.com>
2023-07-26 11:07:04 +0200

    cambios

---
+++ Modulo 81/01 pero del profe/AcmeApp/Acme.Biz/Vendor.cs
@@ -0,0 +1,91 @@
+﻿using Acme.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages the approved vendors from whom Acme purchases our inventory.
+    /// </summary>
+    public class Vendor
+    {
+        public int VendorId { get; set; }
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Sends a product order to the vendor.
+        /// </summary>
+        /// <param name="product">Product to order.</param>
+        /// <param name="quantity">Quantity of the product to order.</param>
+        /// <param name="deliverBy">Requested delivery date.</param>
+        /// <param name="instructions">Delivery instructions.</param>
+        /// <returns></returns>
+        public OperationResult <bool> PlaceOrder(Product product, int quantity,
+                                            DateTimeOffset? deliverBy = null,
+                                            string instructions = "standard delivery")
+        {
+            //+Validaciones al inicio. En clean code se llama:  Falla rápida
+
+            if (product == null) 
+                throw new ArgumentNullException(nameof(product));
+            if (quantity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(quantity));
+            if (deliverBy <= DateTimeOffset.Now)
+                throw new ArgumentOutOfRangeException(nameof(deliverBy));
+
+            var success = false;
+
+            var orderTextBuilder = new StringBuilder("Order from Acme, Inc" +
+                            System.Environment.NewLine +
+                            "Product: " + product.ProductName +
+                            System.Environment.NewLine +
+                            "Quantity: " + quantity); //String builder formula recomendada para trabajar con cadenas.
+                                                      //es +eficiente en memoria
+            if (deliverBy.HasValue)
+            {
+                orderTextBuilder.Append( System.Environment.NewLine +
+                            "Deliver By: " + deliverBy.Value.ToString("d"));
+            }
+            if (!String.IsNullOrWhiteSpace(instructions))
+            {
+                orderTextBuilder.Append( System.Environment.NewLine +
+                            "Instructions: " + instructions);
+            }
+            var orderText = orderTextBuilder.ToString(); //Crear cadena nada mas
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Order", orderText,
+                                                                     this.Email); //Mandamos correo
+            if (confirmation.StartsWith("Message sent:"))
+            {
+                success = true;
+            }
+            var operationResult = new OperationResult<bool>(success, orderText); //Si todo va bien, devolvemos el pedido diciendo además que todo ha ido bien
+            return operationResult;
+        }
+
+        public override string ToString()
+        {
+            return $"Vendor: {this.CompanyName} ({this.VendorId})";
+        }
+
+
+        /// <summary>
+        /// Sends an email to welcome a new vendor.
+        /// </summary>
+        /// <returns></returns>
+        public string SendWelcomeEmail(string message)
+        {
+            var emailService = new EmailService();
+            var subject = ("Hello " + this.CompanyName).Trim();
+            var confirmation = emailService.SendMessage(subject,
+                                                        message,
+                                                        this.Email);
+            return confirmation;
+        }
+    }
+}
---
+++ Modulo 81/01 pero del profe/AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.Common;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+
+        [TestMethod()] //Marcando este método con este atributo
+        public void CalculateSuggestedPriceTest()
+        {
+            // Arrange: Crea nueva instancia del producto
+            var currentProduct = new Product(1, "Saw", ""); //Hemos creado una cadena sierra
+            currentProduct.Cost = 50m;
+            var expected = 55m;
+
+            // Act
+            var actual = currentProduct.CalculateSuggestedPrice(10m);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Product_Null()
+        {
+            //Arrange
+            Product currentProduct = null;
+            var companyName = currentProduct?.ProductVendor?.CompanyName;
+
+            string expected = null;
+
+            //Act
+            var actual = companyName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ProductName_Format()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "  Steel Hammer  ";
+
+            var expected = "Steel Hammer";
+
+            //Act
+            var actual = currentProduct.ProductName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ProductName_TooShort()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "aw";
+
+            string expected = null;
+            string expectedMessage = "Product Name must be at least 3 characters";
+
+            //Act
+            var actual = currentProduct.ProductName;
+            var actualMessage = currentProduct.ValidationMessage;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TestMethod()]
+        public void ProductName_TooLong()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Steel Bladed Hand Saw";
+
+            string expected = null;
+            string expectedMessage = "Product Name cannot be more than 20 characters";
+
+            //Act
+            var actual = currentProduct.ProductName;
+            var actualMessage = currentProduct.ValidationMessage;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TestMethod()]
+        public void ProductName_JustRight() //falso! 3 dig
+        {
+            //Arrange
+            var currentProduct = new Product();//Hemos creado una cadena sierra
+            currentProduct.ProductName = "Saw1"; //falso! 3 dig. prueba con sa
+
+            string expected = "Saw1"; //Lo que espero
+            string expectedMessage = null;
+
+            //Act
+            var actual = currentProduct.ProductName; //devuelveme el producto
+            var actualMessage = currentProduct.ValidationMessage; //devuelveme mensaje de validacion
+
+            //Assert
+            Assert.AreEqual(expected, actual); //comprobamos si es igual el resultado a lo que esppero
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+        //Si pongo null, menos de 3 o 20, error.
+        //poniendo null se detecta la excepcion pero no se rompe nada
+    }
+}
