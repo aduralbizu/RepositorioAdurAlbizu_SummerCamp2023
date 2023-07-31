c4db2ecb0bf6a8e220bdd0c20be2b7714c172efd
aduralbizu <adur.albizu@gmail.com>
2023-07-31 10:11:05 +0200

    nuyeva incorporacion

---
+++ Modulo 15a/Linq/EjemploFuncAction/Ejemplo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploFuncAction
+{
+    internal class Ejemplo
+    {
+        internal void EjemploDelegados1()
+        {
+            // Expresion Lambda mediante Expresion func<>
+
+            Func<string> holaMundoExpresion =
+                //Parametros:
+                ()
+                //operador lamda:
+                =>
+                //Delegado / Método anónimo / Expresión:
+                "Hola Mundo !!!!!!!!";
+
+
+            Console.WriteLine(holaMundoExpresion); //Son diferentes
+            Console.WriteLine(holaMundoExpresion());
+
+            //Extension lambda mediante bloque func<>
+
+            Func<string> holaMundoBloque =
+                //Parametros:
+                ()
+                //operador lamda:
+                =>
+                //Delegado / Método anónimo : No es una expresion!!
+                { 
+                    var cadena = "hola Mundo";
+                    return cadena;
+
+                 };
+
+            Console.WriteLine(holaMundoBloque());
+
+            //ejercicio (calculadora sencilla):
+            //Crear una expresion lambda que sume dos numeros
+
+            //Funcion:
+
+            Func<int,int,string> sumar =
+                //Parametros:
+                (x,y)
+                //operador lamda:
+                =>
+                //Delegado / Método anónimo / Expresión:
+                (x+y).ToString();
+
+
+            Console.WriteLine(sumar(10, 20));
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Modulo 15a/Linq/EjemploMetodosExtension/Program.cs
@@ -0,0 +1,46 @@
+﻿using Extensiones;
+
+namespace EjemploMetodoExtension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string juan = "JuAn lOpEz".ConvertirCadena();
+            string luis = "lUis MaRtinEz".ConvertirCadena();
+
+            var saludo = "hola Mundo!!!";
+            var convertir = saludo.ConvertirCadena();
+
+
+            // Mostrar año de fecha:
+
+            var fecha = DateTime.Now;
+            var año = fecha.ObtenerAño();
+
+            //Agregar metodos de extension para comparar dps enteros.
+
+            int a = 1;
+            int b = 2;
+
+            //bool esmayorAB = a>b
+
+            if (a.Equals(b)) {
+
+                Console.WriteLine("Es mayor");
+            }
+            else
+            {
+                Console.WriteLine("Es menor");
+            }
+
+            //obtener el prier element de la lista
+
+            var lista = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+            var primero = lista.Primero();
+        }
+
+       //Linq crea metodos sobre tipos que están cerrados
+    }
+}
