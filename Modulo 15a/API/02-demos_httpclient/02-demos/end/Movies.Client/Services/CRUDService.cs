647c4afcdde3f71b1f0517e902e924dcc2e2bb71
aduralbizu <adur.albizu@gmail.com>
2023-07-31 13:43:29 +0200

    tarea segunda hora

--- Modulo 15a/API/02-demos_httpclient/02-demos/end/Movies.Client/Services/CRUDService.cs
+++ Modulo 15a/API/02-demos_httpclient/02-demos/end/Movies.Client/Services/CRUDService.cs
@@ -8,13 +8,26 @@ using System.Threading.Tasks;
 
 namespace Movies.Client.Services
 {
+
+
+    //Tarea:
+
+    //1- Crear clase para la respuesta (coger json pegar como clase y ya lo tenemos
+    //2- Crear http client
+    //3- Establecer ruta base por ejempllo http://www.boredapi.com
+    //4- get asinc("api/activity")
+    //5- Deserializar
+
+    //Hemos hecho cambios sobre el documento
+
+
     public class CRUDService : IIntegrationService
     {
 
         public static HttpClient _HttpClient = new HttpClient(); //Hemos creado el cliente
         public CRUDService()
         {
-            _HttpClient.BaseAddress = new Uri("http://localhost:57863");
+            _HttpClient.BaseAddress = new Uri("http://www.boredapi.com");
             
         }
         public async Task Run()
@@ -24,16 +37,30 @@ namespace Movies.Client.Services
         
         }
 
-        private async Task EjemploGet()
+        private async Task EjemploGet() //Hemos editado para el ejercicio
         {
-            var response = await _HttpClient.GetAsync("api/movies"); //une _HttpClient.BaseAddress + "api/movies"
+            var response = await _HttpClient.GetAsync("api/activity?key=5881028"); //une _HttpClient.BaseAddress + "api/movies"
 
             response.EnsureSuccessStatusCode(); //Obligatoria experar a que todo vaya bien sin expecion
 
             var content = await response.Content.ReadAsStringAsync(); //Leer
 
-            var movies = new List<Movie>(); //Hacer la lista
-            movies = JsonConvert.DeserializeObject<List<Movie>>(content); //Desserializar
+            var activity = new Activity(); //Hacer la lista
+            activity = JsonConvert.DeserializeObject<Activity>(content); //Desserializar
+
+            Console.WriteLine("Lectura del JSON completada. JSON extraído:");
+            Console.WriteLine("{");
+            Console.WriteLine($"Activity: {activity.activity}");
+            Console.WriteLine($"Type: {activity.type}");
+            Console.WriteLine($"Participants: {activity.participants}");
+            Console.WriteLine($"Price: {activity.price}");
+            Console.WriteLine($"Link: {activity.link}");
+            Console.WriteLine($"Key: {activity.key}");
+            Console.WriteLine($"Accessibility: {activity.accessibility}");
+            Console.WriteLine("}");
+
+
+            
         }
     }
 }
