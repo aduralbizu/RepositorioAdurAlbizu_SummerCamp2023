e48ccf300e1256937a83ed53c6dcd73a8efd0712
aalbizu-ext <aalbizu-ext@PMPW1361>
2023-08-04 12:15:20 +0200

    rer

---
+++ 4 de agosto/ProyectoFinalConsola/Conversor2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Conversor2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 4 de agosto/ProyectoFinalConsola/Entidades/Moneda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalConsola
+{
+    public class Moneda
+    {
+
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+       
+        public double ValorEnDolares { get; set; } //TODO: Provisional!!!!
+
+
+        public Moneda(string nombre, string codigo, double valorEnDolares)
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            ValorEnDolares = valorEnDolares;
+        }
+    }
+}
