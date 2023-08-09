6fe624b306ef68498d6158895305f4fff555fa04
aduralbizu <adur.albizu@gmail.com>
2023-08-09 11:11:27 +0200

    fdfd

--- 9 de agosto/ProyectoFinalConsola/Conversor/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Conversor.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ 9 de agosto/ProyectosWebSolucion/EjemploMvcConversor/Models/ContextoConversor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace EjemploMvcConversor.Models
+{
+
+    // 1-Desde la consola del administrador de paquetes NuGet escribimos
+    //        add-migration "inicial"
+    // 2-Crear la BBDD mediante el siguiente comando (Tambien desde la consola)
+    //        update-database
+
+
+
+    public class ContextoConversor :DbContext
+    {
+
+        public ContextoConversor(DbContextOptions<ContextoConversor> opciones) : base(opciones)
+        {
+            
+        }
+
+        public DbSet<Moneda> Monedas { get; set; }
+
+    }
+
+    //1- Desde lla concsola del administrados de paquetes nuget
+    //escribimos add-migration "inicial"
+    //2- Crear la bbd mediante el diguiente comanfdo (tambien desddee consola)
+    //update-database
+
+    //herramientas>administrador de pauetes nugget>consola
+    //add-migration inicial. Se creara migration
+
+    //update-database
+}
