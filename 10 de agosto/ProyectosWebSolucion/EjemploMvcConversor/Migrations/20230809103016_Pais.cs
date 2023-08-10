1efaea829a1b63ba0b20388f64b9b634315f9ecf
aduralbizu <adur.albizu@gmail.com>
2023-08-10 09:19:36 +0200

    sd

---
+++ 10 de agosto/ProyectosWebSolucion/EjemploMvcConversor/Migrations/20230809103016_Pais.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EjemploMvcConversor.Migrations
+{
+    public partial class Pais : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "pais",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pais", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pais");
+        }
+    }
+}
---
+++ 10 de agosto/ProyectosWebSolucion/EjemploMvcConversor/Models/Pais.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EjemploMvcConversor.Models
+{
+    public class Pais
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
