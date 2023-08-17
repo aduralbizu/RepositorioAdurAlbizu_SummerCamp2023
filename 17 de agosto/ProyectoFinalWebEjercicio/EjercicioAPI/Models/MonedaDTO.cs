72a82a224357a4c012bd40d99db3d2b1b00086c3
adur.albizu <adur.albizu@gmail.com>
2023-08-17 15:11:06 +0200

    sds

--- 17 de agosto/ProyectoFinalWebEjercicio/EjercicioAPI/Models/MonedaDTO.cs
+++ 17 de agosto/ProyectoFinalWebEjercicio/EjercicioAPI/Models/MonedaDTO.cs
@@ -6,7 +6,7 @@ namespace Models
     //[Table("Moneda")]
     public class MonedaDTO
     {
-        public string Code { get; set; }
+        public string Code { get; set; } 
         public string Name { get; set; }
         public string Symbol { get; set; }
         public string Descripcion { get; set; }
