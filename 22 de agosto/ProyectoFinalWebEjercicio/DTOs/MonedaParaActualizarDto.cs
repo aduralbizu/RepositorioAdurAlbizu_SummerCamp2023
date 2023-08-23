89207771b85de3d8b670cfde435f05ff0d2f92fd
aduralbizu <adur.albizu@gmail.com>
2023-08-23 12:37:00 +0200

    fdf

--- 22 de agosto/ProyectoFinalWebEjercicio/DTOs/MonedaParaActualizarDto.cs
+++ 22 de agosto/ProyectoFinalWebEjercicio/DTOs/MonedaParaActualizarDto.cs
@@ -9,7 +9,6 @@ namespace DTOs
     public class MonedaParaActualizarDto
     {
 
-        public int Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
         public string Symbol { get; set; }
