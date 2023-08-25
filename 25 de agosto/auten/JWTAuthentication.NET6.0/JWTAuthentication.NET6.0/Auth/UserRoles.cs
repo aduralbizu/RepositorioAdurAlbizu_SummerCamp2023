d008470ad0d934cc5208a42fc9d9157c9acc3402
aduralbizu <adur.albizu@gmail.com>
2023-08-25 10:30:10 +0200

    dsds

---
+++ 25 de agosto/auten/JWTAuthentication.NET6.0/JWTAuthentication.NET6.0/Auth/UserRoles.cs
@@ -0,0 +1,12 @@
+﻿namespace JWTAuthentication.NET6._0.Auth
+{
+    //+ Identity (*)
+    //+ JWT (*)
+
+
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
