95bfafdc2d3cf9687b56564a3beddfac338653c1
aduralbizu <adur.albizu@gmail.com>
2023-08-28 14:49:22 +0200

    jj

---
+++ 26 de Agosto/M7 Raw SQL/After/SamuraiApp/SamuraiApp.Data/Migrations/20230825093317_years.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class years : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Years",
+                table: "Samurais",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Years",
+                table: "Samurais");
+        }
+    }
+}
